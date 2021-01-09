7b4c29add4813d9eb05dce49406d38d3fa8cbfd7
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-09 01:45:55 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,11 @@
-# Create your costume_stores migration here
+class CreateCostumes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.string :image_url
+      t.string :size
+      t.timestamps
+    end
+  end
+end
