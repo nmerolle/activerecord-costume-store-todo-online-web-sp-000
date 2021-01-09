09efcfafe55069cf31a4354c7749dc9832247500
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-09 01:49:48 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,7 +6,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.integer :number_of_costumes
       t.integer :number_of_employees
       t.boolean :still_in_business
-      t.timestamps
+      t.timestamp :opening_time
     end
   end
 end
