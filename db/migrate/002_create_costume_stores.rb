0d0fd390f4d2ca07bf84f53b7a91fa25d071c162
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-09 01:50:44 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,7 +3,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
-      t.integer :number_of_costumes
+      t.integer :costumes_inventory
       t.integer :number_of_employees
       t.boolean :still_in_business
       t.timestamp :opening_time
